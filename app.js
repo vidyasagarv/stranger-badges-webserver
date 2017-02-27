99a97020c26250f3bf031653973f0436d9fc1252
vidyasagarv <vidyasagar.039@gmail.com>
2017-02-27 16:43:23 -0500

    changed request url

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ var data = [
 var requestObj = {
     json: data,
     method: 'POST',
-    url: 'http://localhost:8000'
+    url: 'http://stranger-badges.herokuapp.com:80'
 };
 
 (function _request() {
